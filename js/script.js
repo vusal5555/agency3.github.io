ae1b3de4f9594e3db9ebf603ae8404e9e39d8d60
vusal5555 <vusal.novruzov.1@iliauni.edu.ge>
2022-09-18 23:14:44 +0400

    did the same for the other one

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
+// const navLinks = document.getElementById("navLinks");
+// function showMenu() {
+//   navLinks.style.left = "0";
+// }
+// function hideMenu() {
+//   navLinks.style.left = "-300px";
+// }
+
 const navLinks = document.getElementById("navLinks");
-function showMenu() {
-  navLinks.style.left = "0";
-}
-function hideMenu() {
-  navLinks.style.left = "-300px";
-}
+const openMenu = document.querySelector(".fa-solid.fa-bars");
+const closeMenu = document.querySelector(".fa-solid.fa-xmark");
+
+openMenu.addEventListener("click", function () {
+  navLinks.classList.add("hidden");
+});
+closeMenu.addEventListener("click", function () {
+  navLinks.classList.remove("hidden");
+});
